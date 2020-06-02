8a03dda57ce8444653d83c1d233218d9fd3a57e9
Zicheng Wang <wzcwwplyh@163.com>
2020-06-02 22:57:47 +0800

    commit1

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+import {heightRatio, widthRatio} from './styleSheet';
+
+const Constants = {
+  TOP_PIPE_WIDTH: widthRatio * 120,
+  BOTTOM_PIPE_WIDTH: widthRatio * 120,
+  TOP_PIPE_HEIGHT: heightRatio * 120,
+  BOTTOM_PIPE_HEIGHT: heightRatio * 120
+};
+export default Constants;
---
+++ src/utils/random.js
@@ -0,0 +1,6 @@
+export const getRandom = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+export const topObstacleHeight = getRandom(100, 150);
+export const bottomObstacleHeight = getRandom(100, 150);
